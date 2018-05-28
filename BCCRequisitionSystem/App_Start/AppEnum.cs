e3a73475f78999be89454e18ba519e986e761cd2
parvezahmed500 <parvezfci@gmail.com>
2018-05-28 16:32:29 +0600

    Mvc project added

---
+++ BCCRequisitionSystem/App_Start/AppEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BCCRequisitionSystem
+{
+    public class AppEnum
+    {
+        public enum RolEnum
+        {
+            Admin=0,
+            User=1,
+            VehicleController=2
+        }
+    }
+}
---
+++ BCCRequisitionSystem/RequisitionManagement.Models/ViewModels/EmployeeViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RequisitionManagement.Models.EntityModels;
+
+namespace RequisitionManagement.Models.ViewModels
+{
+    public class EmployeeViewModels
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DesignationId { get; set; }
+        public Designation Designation { get; set; }
+        public string Address { get; set; }
+        public string Nid { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
