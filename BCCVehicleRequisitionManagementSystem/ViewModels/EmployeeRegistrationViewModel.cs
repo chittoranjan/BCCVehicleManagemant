cb9081712988b1f1d590b80b9c5a0878ac6ae20c
chittoranjan <chittoranjan.sarker@gmail.com>
2018-07-03 16:18:49 +0600

    employee info added

---
+++ BCCVehicleRequisitionManagementSystem/ViewModels/EmployeeRegistrationViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BCCVehicleRequisitionManagementSystem.Models.EntityModels;
+
+namespace BCCVehicleRequisitionManagementSystem.ViewModels
+{
+    public class EmployeeProfileViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please input employee name!")]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select employee designation!")]
+        [Display(Name = "Employeee Designation")]
+        public int EmployeeDesignationId { get; set; }
+        public EmployeeDesignation EmployeeDesignation { get; set; }
+
+        [Required(ErrorMessage = "please select Deparment")]
+        [Display(Name = "Deparment Name")]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        [Required(ErrorMessage = "Please input mobile no!")]
+        [Display(Name = "Contact No")]
+        public string ContactNo { get; set; }
+        
+        [Display(Name = "Present Address")]
+        [Required(ErrorMessage = "Please input present address!")]
+        public string Address { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
